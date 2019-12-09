8243edf6d2a8613e0c00126f724a300af7fe09b6
Alexandre <alexandre.laine@student.ecp.fr>
2019-12-09 15:27:37 +0100

    Crash testing

---
+++ server/models/routes.py
@@ -0,0 +1,26 @@
+from flask import request, render_template, make_response
+from datetime import datetime as dt
+from flask import current_app as app
+from .model import db, User
+
+
+@app.route('/', methods=['GET'])
+def create_user():
+    """Create a user."""
+    username = request.args.get('user')
+    email = request.args.get('email')
+    if username and email:
+        existing_user = User.query.filter(User.username == username or User.email == email).first()
+        if existing_user:
+            return make_response(f'{username} ({email}) already created!')
+        new_user = User(username=username,
+                        email=email,
+                        created=dt.now(),
+                        bio="In West Philadelphia born and raised, on the playground is where I spent most of my days",
+                        admin=False)  # Create an instance of the User class
+        db.session.add(new_user)  # Adds new User record to database
+        db.session.commit()  # Commits all changes
+    users = User.query.all()
+    return render_template('users.html',
+                           users=users,
+                           title="Show Users")
