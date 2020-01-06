0d966eba94af68f0f74d88fa5fa63cdc6e40e6f0
Pierre Averous <piaverous@gmail.com>
2020-01-06 16:20:47 +0100

    Fix frontend finally

--- client/internals/webpack/webpack.prod.babel.js
+++ client/internals/webpack/webpack.prod.babel.js
@@ -6,6 +6,7 @@ const OfflinePlugin = require('offline-plugin');
 const { HashedModuleIdsPlugin } = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = require('./webpack.base.babel')({
   mode: 'production',
@@ -142,6 +143,7 @@ module.exports = require('./webpack.base.babel')({
       hashDigest: 'hex',
       hashDigestLength: 20,
     }),
+    new Dotenv(),
   ],
 
   performance: {
--- client/start.sh
+++ client/start.sh
@@ -3,6 +3,6 @@ ENVFILE="./.env"
 if test -f "$ENVFILE"; then
     echo "Found $ENVFILE file"
 else
-  echo "SERVER_URL = $SERVER_URL" >> .env
-  echo "SOCKET_URL = $SOCKET_URL" >> .env
+  echo "SERVER_URL=$SERVER_URL" >> .env
+  echo "SOCKET_URL=$SOCKET_URL" >> .env
 fi
