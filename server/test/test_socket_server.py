96654199447593eef33c35fcb9fd7fa51abbca70
bloussou <brieuc.loussouarn@orange.fr>
2020-01-29 22:13:44 +0100

    set up tests class

--- server/test/test_socket_server.py
+++ server/test/test_socket_server.py
@@ -1,14 +1,38 @@
 """Test socket server call."""
 
 from flask_socketio import SocketIOTestClient, test_client
-# from app import app, socketio
 # from socket_module.socket_server import SocketServer
 from app import app, socketio
+from socket_module.socket_messages import socket_messages
 
 
-def test_socket_connection():
-    """Test socket connection is working properly."""
-    client = socketio.test_client(app)
-    assert client.is_connected()
-    received = client.get_received()
-    print(received)
+class TestClassWithClient:
+    """Base class containing a socket client instance."""
+
+    @classmethod
+    def setup_class(cls):
+        """Create the client."""
+        cls.client = socketio.test_client(app)
+
+class TestSocketServer(TestClassWithClient):
+    """Test Class for socket server."""
+
+    def test_socket_connection(self):
+        """Test socket connection is working properly."""
+        assert self.client.is_connected()
+        received = client.get_received()
+        print(received)
+
+    def test_join_jap(self):
+        """Test join jap."""
+        self.client.emit(socket_messages["JOIN_JAP"], {"user_id":0, "jap_event_id":1})
+        # client = socketio.test_client(
+        #     app, query_string=socket_messages["JOIN_JAP"])
+        received = client.get_received()
+        print(received)
+        # self.assertEqual(len(received), 3)
+        # self.assertEqual(received[0]['args'], 'connected')
+        # self.assertEqual(received[1]['args'], '{"foo": ["bar", "baz"]}')
+        # self.assertEqual(received[2]['args'],
+        #                  '{"Authorization": "Bearer foobar"}')
+        # client.disconnect()
