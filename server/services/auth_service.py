b76ac3613be57a92b61fbc561059f61cc2b007b3
Pierre Averous <piaverous@gmail.com>
2020-02-03 14:40:52 +0100

    Fix docstring

--- server/services/auth_service.py
+++ server/services/auth_service.py
@@ -3,8 +3,14 @@ from services import UserService
 
 class AuthService:
     """A service used to log in to the app."""
+
     @staticmethod
     def login(username):
+        """Logs a user in, using his username.
+
+        Args :
+            username: string
+        """
         user = UserService.get_user_by_name(username)
         if not user:
             return AuthService.register(username)
@@ -12,5 +18,10 @@ class AuthService:
 
     @staticmethod
     def register(username):
+        """Registers a new user, with his username.
+
+        Args :
+            username: string
+        """
         user = UserService.create_user(username, username, username, None)
         return user
